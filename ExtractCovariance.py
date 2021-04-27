Stashing changes. Updated pynpoint outputs to match pyklip, and fixed covariance read in.

ExtractCovariance.py
@@ -46,14 +46,15 @@ def main(args):
     parser.add_argument("name", type=str, default= "HR8799")   
     args = parser.parse_args(args)
 
-    if "gpi" in instrument.lower():
-        pxscale = 0.014161
-    elif "sphere" in instrument.lower():
-        pxscale = 0.007462
+
 
     data_dir = args.path
     instrument = args.instrument
     planet_name = args.name
+    if "gpi" in instrument.lower():
+        pxscale = 0.014161
+    elif "sphere" in instrument.lower():
+        pxscale = 0.007462
     # Setup directories
     if not data_dir.endswith("/"):
         data_dir += "/"
@@ -71,17 +72,27 @@ def main(args):
         residuals = np.load(data_dir + instrument + "_" + planet_name + "_residuals.npy")
     elif os.path.exists(data_dir + instrument + "_" + planet_name + "_residuals.fits"):
         hdul = fits.open(data_dir + instrument + "_" + planet_name + "_residuals.fits")
-        residuals = [hdu.data for hdu in hdul[1:]]
-        if "gpi" in instrument.lower():
-            try: 
-                CENTER = (hdul[1].header['PSFCENTX'],hdul[1].header['PSFCENTX'])
-            except:
-                CENTER = (hdul[0].header['PSFCENTX'],hdul[0].header['PSFCENTX'])
+        if "pynpoint" in data_dir:
+            dataind = 0
         else:
-            CENTER = (residual.shape[-2]/2.0,residual.shape[-1]/2.0)
+            dataind = 1
+        residuals = np.array([hdu.data for hdu in hdul[dataind:]])
+        if "pynpoint" in data_dir:
+            residuals = residuals[0]
+        print(residuals.shape)
     else:
         print("No residual file found!")
         return
+
+    try: 
+        CENTER = (hdul[1].header['PSFCENTX'],hdul[1].header['PSFCENTX'])
+    except:
+        try: 
+            CENTER = (hdul[1].header['PSFCENTX'],hdul[1].header['PSFCENTX'])
+        except:
+            CENTER = (residuals.shape[-2]/2.0,residuals.shape[-1]/2.0) 
+        
+
     residuals = np.array(residuals)
     print(spectrum.shape,residuals.shape)
 
@@ -260,7 +271,8 @@ def uncorrelated_error(residuals,spectrum,nl,npca=None,flux_cal = False):
         nFrames = psf_cube.shape[1]
         psf_cube = np.nanmean(psf_cube,axis=1)
     elif instrument.lower() == "sphereyjh":
-        psf_cube = fits.open(glob.glob(data_dir + psf_name)[0])
+        psf_name = "../psf_cube.fits"
+        psf_cube = fits.open(glob.glob(data_dir + psf_name)[0])[0].data
         nFrames = 1
     elif "gpi" in instrument.lower():
         psf_name = glob.glob(data_dir + "../*_PSF_cube.fits")[0]
@@ -332,7 +344,8 @@ def uncorrelated_error(residuals,spectrum,nl,npca=None,flux_cal = False):
         uncor_err = np.array(uncor_err)
         total_err = np.array(total_err)
         real_err = total_err*spectrum
-    del dataset
+    if "gpi"in instrument.lower():
+        del dataset
     return real_err, total_err, uncor_err, fluxes
 
 def photometric_error(psf_cube):
@@ -345,7 +358,7 @@ def photometric_error(psf_cube):
     bkgs = []
     flux = []
     errdict = {"GPIH": 60,"GPIK1":24,"GPIK2":20,"SPHEREYJH":72}
-
+    print(psf_cube.shape)
     for frame in psf_cube[:]:
         y_img, x_img = np.indices(frame.shape, dtype=float)
         r_img = np.sqrt((x_img - frame.shape[0]/2.0)**2 + (y_img - frame.shape[1]/2.0)**2)

KLIPExtractSpectrum.py
@@ -122,11 +122,21 @@ def main(args):
     else: 
         posn_dict = read_astrometry(data_dir,planet_name)
         posn = (posn_dict["Separation [mas]"][0], posn_dict["PA [deg]"][0])
+
     if not args.cont:
+        # Run klip
         exspect, fm_matrix = KLIP_Extraction(dataset, PSF_cube, posn, numthreads)
+        # Get flux in flux and contrast units
         contrasts,flux = get_spectrum(dataset, exspect,spot_to_star_ratio, stellar_model)
+        # Get full frame residuals. Not strictly correct, but should be ok
         KLIP_fulframe(dataset, PSF_cube, posn, numthreads)
+    else:
+        exspect = np.load(data_dir + "/pyklip/exspect.npy")
+    # Generate nice outputs
     combine_residuals()
+
+    # Find scaling factors for oversubtraction
+    mcmc_scaling(dataset,PSF_cube,posn,exspect,spot_to_star_ratio,stellar_model)
     del dataset
     return 
 
@@ -155,7 +165,7 @@ def init_sphere():
                          fitsinfo,
                          wvinfo, 
                          nan_mask_boxsize=9,
-                         psf_cube_size = 13)
+                         psf_cube_size = 15)
     print("read in data")
     return dataset
 
@@ -228,6 +238,9 @@ def KLIP_Extraction(dataset, PSF_cube, posn, numthreads):
     ###### The forward model class ######
     # WATCH OUT FOR MEMORY ISSUES HERE
     # If the PSF size, input size or numbasis size is too large, will cause issues on cluster
+    dtype = "double"
+    if "sphere" in instrument.lower():
+        dtype = "float"
     fm_class = es.ExtractSpec(dataset.input.shape,
                         numbasis,
                         planet_sep,
@@ -235,7 +248,7 @@ def KLIP_Extraction(dataset, PSF_cube, posn, numthreads):
                         PSF_cube,
                         np.unique(dataset.wvs),
                         stamp_size = stamp_size,
-                        datatype = 'double') #must be double
+                        datatype = dtype) #must be double?
 
     ###### Now run KLIP! ######
     fm.klip_dataset(dataset, fm_class,
@@ -363,22 +376,29 @@ def KLIP_fulframe(dataset, PSF_cube, posn, numthreads):
                     mute_progression=True)
     return 
 
-def recover_fake(dataset, files, position, fake_flux, kklip):
+def recover_fake(dataset, PSF_cube, files, position, fake_flux, kklip):
     # We will need to create a new dataset each time.
-    nbasis = [kklip]
+    N_frames = len(dataset.input)
+    N_cubes = np.size(np.unique(dataset.filenums))
+    nl = N_frames // N_cubes
+
     # PSF model template for each cube observation, copies of the PSF model:
     inputpsfs = np.tile(dataset.psfs, (N_cubes, 1, 1))
     bulk_contrast = 1e-2
     fake_psf = inputpsfs*fake_flux[:,None,None]*dataset.dn_per_contrast[:,None,None]
-    pa = position[1]
-    tmp_dataset = GPI.GPIData(files, highpass=False, PSF_cube = psf,recalc_centers=True,meas_satspot_flux=True)
-    tmp_dataset.generate_psf_cube(21)
+    planet_sep, pa = position
+    planet_sep =planet_sep/1000 / pxscale #mas to pixels
+    if "sphere" in instrument.lower():
+        tmp_dataset = init_sphere()
+    if "gpi" in instrument.lower():
+        tmp_dataset = init_gpi()
+        tmp_dataset.generate_psf_cube(21)
     print(position)
     fakes.inject_planet(tmp_dataset.input, tmp_dataset.centers, fake_psf,\
                                     tmp_dataset.wcs, planet_sep, pa)
 
     fm_class = es.ExtractSpec(tmp_dataset.input.shape,
-                               nbasis,
+                               numbasis[kklip],
                                planet_sep,
                                pa,
                                PSF_cube,
@@ -393,7 +413,7 @@ def recover_fake(dataset, files, position, fake_flux, kklip):
                                       (pa+2.0*stamp_size)/180.*np.pi]],
                         movement=movement,
                         #flux_overlap = 0.1,
-                        numbasis = numbasis[k],
+                        numbasis = numbasis[kklip],
                         maxnumbasis=maxnumbasis,
                         numthreads=numthreads,
                         spectrum=None,#spectra_template,
@@ -408,7 +428,7 @@ def recover_fake(dataset, files, position, fake_flux, kklip):
     del tmp_dataset
     return fake_spect
 
-def mcmc_scaling(dataset,posn,exspect,spot_to_star_ratio,stellar_model):
+def mcmc_scaling(dataset,PSF_cube,posn,exspect,spot_to_star_ratio,stellar_model):
     print("Running MCMC to compute over-subtraction scaling factor.")
 
     files = glob.glob(data_dir + "*corr.fits")
@@ -431,7 +451,7 @@ def mcmc_scaling(dataset,posn,exspect,spot_to_star_ratio,stellar_model):
         input_spect = np.tile(exspect[k,:]*spot_to_star_ratio, N_cubes)
         fake_spectra = np.zeros((npas, nl))
         for p, para in enumerate(pas):
-            fake_spectra[p,:] = recover_fake(dataset, files, (planet_sep, para), input_spect, numbasis[k])
+            fake_spectra[p,:] = recover_fake(dataset, PSF_cube, files, (planet_sep, para), input_spect, k)
             scaling.append((exspect[k,:]*spot_to_star_ratio)/(fake_spectra[p,:]/dataset.dn_per_contrast[:nl]))
     np.save(data_dir + "pyklip/mcmc_outputs",fake_spectra)
 
@@ -442,7 +462,7 @@ def mcmc_scaling(dataset,posn,exspect,spot_to_star_ratio,stellar_model):
 
 def combine_residuals():
     print("Combining residuals into fits file.")
-    files = sorted(glob.glob(data_dir + "pyklip/*fullframe*"))
+    files = sorted(glob.glob(data_dir + "pyklip/fullframe*"))
     hduls = []
     hdu0 = fits.PrimaryHDU()
     hdul = fits.open(files[0])
@@ -450,13 +470,17 @@ def combine_residuals():
     hdul.close()
 
     hduls.append(hdu0)
+    if "sphere" in instrument.lower():
+        dataind = 0 # For some reason the outputs for the fm are different
+    elif "gpi" in instrument.lower():
+        dataind = 1
     for i,f in enumerate(files):
         if "KLmodes" in f:
             continue
         hdul = fits.open(f)
-        data = hdul[1].data
+        data = hdul[dataind].data
         hdu = fits.ImageHDU(data,name = str(numbasis[i])+"PC")
-        hdu.header = hdul[1].header
+        hdu.header = hdul[dataind].header
         hduls.append(hdu)
         hdul.close()
 

PynpointExtractSpectrum.py
@@ -126,14 +126,15 @@ def main(args):
     data_shape = preproc_files()
 
     # Check for KLIP astrometry and either read in or create
-    if not os.path.exists(data_dir + "pyklip/"+ planet_name + "_astrometry.txt"):
-        if "gpi" in instrument.lower():
-            dataset = init_gpi(data_dir)
-        elif "sphere" in instrument.lower():
-            dataset = init_sphere(data_dir)
-        PSF_cube,cal_cube,spot_to_star_ratio = init_psfs(dataset)
-        # posn is in sep [mas] and PA [degree], we need offsets in x and y px
-        posn = get_astrometry(dataset, PSF_cube, guesssep, guesspa, guessflux,data_dir,planet_name)
+    if not cont:
+        if not os.path.exists(data_dir + "pyklip/"+ planet_name + "_astrometry.txt"):
+            if "gpi" in instrument.lower():
+                dataset = init_gpi(data_dir)
+            elif "sphere" in instrument.lower():
+                dataset = init_sphere(data_dir)
+            PSF_cube,cal_cube,spot_to_star_ratio = init_psfs(dataset)
+            # posn is in sep [mas] and PA [degree], we need offsets in x and y px
+            posn = get_astrometry(dataset, PSF_cube, guesssep, guesspa, guessflux,data_dir,planet_name)
 
     # read_astrometry gives offsets in x,y, need to compute absolute posns
     posn_dict = read_astrometry(data_dir,planet_name)
@@ -142,12 +143,12 @@ def main(args):
 
     # Sanity chec the posn
     print(CENTER,posn,posn_pyn)
-    if not cont:
-        # Run ADI for each channel individually
-        run_all_channels(nChannels,
-                        base_name,
-                        instrument + "_" + planet_name,
-                        posn_pyn)
+    #if not cont:
+    # Run ADI for each channel individually
+    run_all_channels(nChannels,
+                    base_name,
+                    instrument + "_" + planet_name,
+                    posn_pyn)
 
     # Save outputs to numpy arrays
     contrasts = save_contrasts(nChannels,
@@ -180,6 +181,7 @@ def test_analysis(input_name,psf_name,output_name,posn,working_dir,waffle = Fals
                                filenames = sorted(glob.glob(data_dir + input_name)))
     test_pipeline.add_module(module)
 
+    #todo - generic naming for wavelength
     module = WavelengthReadingModule(name_in="read_wlen",
                                input_dir=data_dir,
                                data_tag="science",
@@ -285,7 +287,7 @@ def simplex_one_channel(channel,input_name,psf_name,output_name,posn,working_dir
                                        aperture = 0.04, # in arcsec
                                        tolerance = 0.0005, # tighter tolerance is good
                                        pca_number = pcas, #listed above
-                                       cent_size = 0.15, # how much to block out 
+                                       cent_size = 0.2, # how much to block out 
                                        offset = 1.0) #use fixed astrometry from KLIP
 
     pipeline.add_module(module)
@@ -297,16 +299,16 @@ def simplex_one_channel(channel,input_name,psf_name,output_name,posn,working_dir
         save_residuals(residuals[-1],output_name +"_residuals_" + str(channel).zfill(3) + "_pca_" + str(pca), data_dir+ "pynpoint/" )
 
 # Run Pynpoint
-def run_all_channels(nChannels, base_name, output_name, posn, skip = False):
+def run_all_channels(nChannels, base_name, output_name, posn, skip = True):
     global pcas
     if not isinstance(pcas, list): 
         pcas = pcas.tolist()
-    if "sphere" in instrument.lower():
+    """if "sphere" in instrument.lower():
         # use 'frames removed' files
         test_analysis("frames_removed.fits","*_PSF.fits",output_name,posn,data_dir)
     elif "gpi" in instrument.lower():
         # reshape data into a single file to read in for correct shape
-        test_analysis("*pyklip_frames_removed.fits","*_PSF.fits",output_name,posn,data_dir)
+        test_analysis("*pyklip_frames_removed.fits","*_PSF.fits",output_name,posn,data_dir)"""
 
     # Loop over all channels
     for channel in range(nChannels):
@@ -471,7 +473,7 @@ def preproc_files(skip = False):
         cube = hdul[0].data
         CENTER = (cube.shape[-2]/2.0,cube.shape[-1]/2.0)
         NORMFACTOR = DIT_SCIENCE/DIT_FLUX
-        N_cubes =np.size(np.unique(dataset.filenums))
+        N_cubes = cube.shape[1]
         pcas[pcas>=N_cubes] = N_cubes -1
         pcas = np.unique(pcas).tolist()
         # Data shape is used to calculate image center, so it's returned